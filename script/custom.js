dfa38d1dbf3e52eb470c72ad14c2e0fb1088ee63
juhyun <kjhabc2002@gmail.com>
2020-09-18 05:15:26 +0900

    modified #1

--- script/custom.js
+++ script/custom.js
@@ -1,21 +1,41 @@
-var slideIndex = 0;
-showSlides();
+// 메인화면 carosel
+var slideIndex = 1;
+showSlides(slideIndex);
 
-function showSlides() {
+function plusSlides(n) {
+  showSlides((slideIndex += n));
+}
+
+function currentSlide(n) {
+  showSlides((slideIndex = n));
+}
+
+function showSlides(n) {
   var i;
-  var slides = document.getElementsByClassName("mySlides");
+  var contents = document.getElementsByClassName("content");
   var dots = document.getElementsByClassName("dot");
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-  slideIndex++;
-  if (slideIndex > slides.length) {
+  if (n > contents.length) {
     slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = contents.length;
+  }
+  for (i = 0; i < contents.length; i++) {
+    contents[i].style.display = "none";
   }
   for (i = 0; i < dots.length; i++) {
     dots[i].className = dots[i].className.replace(" active", "");
   }
-  slides[slideIndex - 1].style.display = "block";
+  contents[slideIndex - 1].style.display = "block";
   dots[slideIndex - 1].className += " active";
-  setTimeout(showSlides, 5000); // Change image every 2 seconds
+  setTimeout(showSlides, 2000); // Change image every 2 seconds
 }
+
+//해시태그 탭메뉴 콘텐츠
+$(".btn li").click(function () {
+  $(this).addClass("active");
+  $(this).siblings("li").removeClass("active");
+  var tab = $(this).attr("data_alt");
+  $(".tabs div").removeClass("active");
+  $("#" + tab).addClass("active");
+});
